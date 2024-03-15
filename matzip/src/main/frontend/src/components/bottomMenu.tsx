e75046caeb123c21e4fe3fbe9925a774e159b312
chloe_choi <plumxeun@gmail.com>
2024-03-16 01:14:30 +0900

    btm menu navigate attatched

--- matzip/src/main/frontend/src/components/bottomMenu.tsx
+++ matzip/src/main/frontend/src/components/bottomMenu.tsx
@@ -3,17 +3,21 @@ import {MapButton, MenuContainer, RecordButton, SearchButton} from "./styled";
 import searchIcon from '../img/iconSearch.svg'
 import mapIcon from '../img/iconMap.svg'
 import recordIcon from '../img/iconRecord.svg'
+import { useNavigate } from "react-router-dom";
 
 export default function BottomMenu(){
+    
+    let navigate= useNavigate()
+    
     return(
         <MenuContainer>
-            <SearchButton>
+            <SearchButton onClick={()=>{navigate('/search')}}>
                 <img src={searchIcon} alt=''></img>
             </SearchButton>
-            <MapButton>
+            <MapButton onClick={()=>{navigate('/map')}}>
                 <img src={mapIcon} alt=''></img>
             </MapButton>
-            <RecordButton>
+            <RecordButton onClick={()=>{navigate('/record')}}>
                 <img src={recordIcon} alt=''></img>
             </RecordButton>
         </MenuContainer>
