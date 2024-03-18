db48f1fbd7cbc97741055951012429d3a60b38f7
chloe_choi <plumxeun@gmail.com>
2024-03-18 20:27:34 +0900

    Search Page Basic UI

--- matzip/src/main/frontend/src/pages/Home/index.tsx
+++ matzip/src/main/frontend/src/pages/Home/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import {useNavigate} from "react-router-dom";
+import BottomMenu from "../../components/bottomMenu";
 
 export default function Home() {
+    let navigate= useNavigate()
   return (
     <div>
       <h1>Home Page</h1>
       <p>일단 공용페이지로 나눠놓음</p>
+        <Navbar>
+          <Container>
+            <Nav>
+              <Nav.Link onClick={()=>{navigate(-1)}}>rollback</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <BottomMenu></BottomMenu>
+
+
     </div>
   );
 }
